b602c25a7cd047533ab29c5521c6bd3364d4a17d
Elliott Grieco <elliott.grieco@gmail.com>
2017-11-07 17:14:42 -0500

    Implements points-aware AI

--- players/ai_player.rb
+++ players/ai_player.rb
@@ -11,10 +11,37 @@ class AIPlayer < Player
     Knight: 30,
     Pawn: 10,
   }.freeze
-
+  
   def play_turn(board)
-    random_move = all_moves(board).sample
-    board.move_piece(*random_move)
+    best_move = [nil, nil]
+    best_move_points = -9999
+
+    all_moves(board).each do |test_move|
+      test_board = board.dup
+      test_board.move_piece!(*test_move)
+      points = evaluate(test_board)
+      if points > best_move_points
+        best_move = test_move
+        best_move_points = points
+      elsif points === best_move_points
+        best_move = [test_move, best_move].sample
+      end
+    end
+
+    board.move_piece(*best_move)
+  end
+
+  def evaluate(board)
+    opponent_color = color == :black ? :white : :black
+    points = player_values(board, color).inject(:+)
+    points - player_values(board, opponent_color).inject(:+)
+  end
+
+  def player_values(board, color)
+    board.all_pieces(color).map do |piece|
+      key = piece.class.to_s.to_sym
+      PIECE_VALUES[key]
+    end
   end
 
   def all_moves(board)
