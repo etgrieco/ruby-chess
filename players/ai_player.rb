caee6cdb98cd21d781dd020ad02e6756a19d8fac
Elliott Grieco <elliott.grieco@gmail.com>
2017-11-07 20:02:38 -0500

    First attempt minimax

--- players/ai_player.rb
+++ players/ai_player.rb
@@ -13,7 +13,8 @@ class AIPlayer < Player
   }.freeze
 
   def play_turn(board)
-    best_move = get_best_move(board)
+    # best_move = get_best_simple_move(board)
+    best_move = get_best_minimax_move(2, board)
     board.move_piece(*best_move)
   end
 
@@ -58,6 +59,46 @@ class AIPlayer < Player
       moves.push(*p_moves)
     end
     moves
+  end
+
+
+  def get_best_minimax_move(depth, board)
+    best_move = { move: [nil, nil], points: -9999 }
+
+    all_moves(board).each do |test_move|
+      test_board = create_test_board(test_move, board)
+      points = minimax(depth, test_board, self.color)
+      if points > best_move[:points]
+        best_move = { move: test_move, points: points }
+      end
+    end
+
+    best_move[:move]
+  end
+
+  def minimax(depth, board, maximizing_color)
+    opponent_color = maximizing_color == :black ? :white : :black
+
+    if depth == 0
+      return evaluate(board)
+    end
+
+    all_moves = all_moves(board)
+    if maximizing_color == color
+      best_move = -9999
+      all_moves.each do |move|
+        test_board = create_test_board(move, board)
+        best_move = [best_move, minimax(depth - 1, test_board, opponent_color)].max
+      end
+    else
+      best_move = 9999
+      all_moves.each do |move|
+        test_board = create_test_board(move, board)
+        best_move = [best_move, minimax(depth - 1, test_board, opponent_color)].min
+      end
+    end
+
+    best_move
   end
 
 end
